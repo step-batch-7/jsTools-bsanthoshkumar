bf09e3f2e056e74b85ef8f099d0488caafb1ec85
santhosh <santhosh.kumar@thoughtworks.com>
2019-12-23 09:08:50 +0530

    added new function extractFileContent
    What changes you have made?
    added new function extractFileContent in cutLib.js
    
    added test cases for extractFileContent function
    
    modified todo

--- src/cutLib.js
+++ src/cutLib.js
@@ -2,4 +2,12 @@ const joinExtractedLines = function(extrcatedLines) {
   return extrcatedLines.join("\n");
 };
 
-module.exports = { joinExtractedLines };
+const extractFileContent = function(fileContent, userOptions) {
+  const { fields } = userOptions;
+  let extractedContent = fileContent.map(line => {
+    line = line.split(",");
+    return line[fields - 1];
+  });
+  return extractedContent;
+};
+module.exports = { joinExtractedLines, extractFileContent };
--- test/testCutLib.js
+++ test/testCutLib.js
@@ -1,10 +1,20 @@
 const assert = require("assert");
-const { joinExtractedLines } = require("../src/cutLib");
+const { joinExtractedLines, extractFileContent } = require("../src/cutLib");
 
 describe("joinExtractedLines", function() {
   it("should give string format of given line", function() {
     const extractedLines = ["abc", "def"];
     const expected = "abc\ndef";
     assert.deepStrictEqual(joinExtractedLines(extractedLines), expected);
+  });
+});
+
+describe("extractFileContent", function() {
+  it("should give extracted content of each line", function() {
+    const fileContent = ["a,b,c,d,e", "f,g,h,i,j", "k,l,m,n,o"];
+    const userOptions = { fields: 5 };
+    const actual = extractFileContent(fileContent, userOptions);
+    const expected = ["e", "j", "o"];
+    assert.deepStrictEqual(actual, expected);
   });
 });
