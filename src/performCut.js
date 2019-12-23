5c428c0f5b4c2b6ef70df87b9b5a4be1d23e2184
santhosh <santhosh.kumar@thoughtworks.com>
2019-12-23 14:42:50 +0530

    added testcase for performCutOperations.js
    What changes you have made?
    modified performCut.js
    added one test case for performCutOperation.js

--- src/performCut.js
+++ src/performCut.js
@@ -7,6 +7,8 @@ const {
 
 const performCutOperation = function(args, fileSys) {
   const userOptions = parseCmdLineArgs(args);
+  if (!fileSys.existsFile(userOptions.filePath))
+    return `cut: ${userOptions.filePath}: No such File or Directory`;
   const fileContent = readFileContent(fileSys, userOptions.filePath);
   const extractedContent = extractFileContent(fileContent, userOptions);
   return joinExtractedLines(extractedContent);
--- test/testPerformCut.js
+++ test/testPerformCut.js
@@ -6,9 +6,22 @@ describe("performCutOpertion", function() {
     const readFile = (filePath, encoding) => {
       return "1 2 3 4 5\n11 12 13 14 15\n21 22 23 24 25\n31 32 33 34 35\n41 42 43 44 45";
     };
-    const fileSys = { readFile: readFile, encoding: "utf8" };
+    const existsFile = filePath => true;
+    const fileSys = {
+      readFile: readFile,
+      existsFile: existsFile,
+      encoding: "utf8"
+    };
     const args = ["-f", "3", "./sampleText.txt"];
     const expected = "3\n13\n23\n33\n43";
+    assert.strictEqual(performCutOperation(args, fileSys), expected);
+  });
+
+  it("should give error for wrong filePath", function() {
+    const existsFile = filePath => false;
+    const fileSys = { existsFile: existsFile };
+    const args = ["-f", "3", "./sampleText.txt"];
+    const expected = `cut: ./sampleText.txt: No such File or Directory`;
     assert.strictEqual(performCutOperation(args, fileSys), expected);
   });
 });
