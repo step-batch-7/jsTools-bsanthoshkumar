53a350a826ec664527892b8f120bb8506fb0a676
santhosh <santhosh.kumar@thoughtworks.com>
2019-12-24 12:15:15 +0530

    changed functions to arrow functions
    What changes you have made?
    changed functions to arrow functions

--- src/cutLib.js
+++ src/cutLib.js
@@ -1,16 +1,16 @@
-const parseCmdLineArgs = function(args) {
+const parseCmdLineArgs = args => {
   const fields = args[args.indexOf("-f") + 1].split(",");
   const delimiter = args[args.indexOf("-d") + 1];
   const filePath = args[args.length - 1];
   return { filePath: filePath, fields: fields, delimiter: delimiter };
 };
 
-const readFileContent = function(fileSys, path) {
+const readFileContent = (fileSys, path) => {
   const fileContent = fileSys.readFile(path, fileSys.encoding);
   return fileContent.split("\n");
 };
 
-const extractFileContent = function(fileContent, userOptions) {
+const extractFileContent = (fileContent, userOptions) => {
   const { fields } = userOptions;
   if (+fields == 0)
     return { error: "cut: [-cf] list: values may not include zero" };
@@ -19,7 +19,7 @@ const extractFileContent = function(fileContent, userOptions) {
   return { extractedLines };
 };
 
-const extractFieldOfLine = function(line) {
+const extractFieldOfLine = line => {
   if (!line.includes(this.delimiter)) return line;
   line = line.split(this.delimiter);
   return line[this.fields[0] - 1];
@@ -27,7 +27,7 @@ const extractFieldOfLine = function(line) {
 
 const joinExtractedLines = extrcatedLines => extrcatedLines.join("\n");
 
-const performCutOperation = function(args, fileSys) {
+const performCutOperation = (args, fileSys) => {
   const userOptions = parseCmdLineArgs(args);
   const { filePath } = userOptions;
   if (!fileSys.existsFile(filePath))
