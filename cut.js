f4da31809555271c0f1e6bc49c88b8071fcbd14e
santhosh <santhosh.kumar@thoughtworks.com>
2019-12-31 12:43:07 +0530

    modified cut.js
    What changes you have made?
    changed slicing procees.argv to destructuring

--- cut.js
+++ cut.js
@@ -3,15 +3,14 @@ const { createReadStream } = require('fs');
 const { stdin, stdout, stderr } = process;
 const { cut } = require('./src/performCut');
 
-const userOptionsStartIndex = 2;
-
 const write = resultOfCut => {
   stdout.write(resultOfCut.lines);
   stderr.write(resultOfCut.error);
 };
 
-const main = function(userOptions) {
+const main = function(cmdLineArgs) {
+  const [, , ...userOptions] = cmdLineArgs;
   cut(userOptions, { createReadStream, stdin }, write);
 };
 
-main(process.argv.slice(userOptionsStartIndex));
+main(process.argv);
