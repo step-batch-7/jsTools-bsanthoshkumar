42264893ff04827d0a6712b5c7e1a637cea601db
santhosh <santhosh.kumar@thoughtworks.com>
2020-01-02 14:14:55 +0530

    added gitHooks.sh

--- addGitHooks.sh
+++ addGitHooks.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 
 cat <<EOF > .git/hooks/pre-commit  
-eslint src test *.js
+npm run allTests && npm run linter
 if [ \$? != 0 ]; then 
     exit 1
 fi
 EOF
 
-
-chmod +x .git/hooks/pre-commit  
-
-cat <<EOF > .git/hooks/pre-push  
-npm run getLinter 
-if [ \$? != 0 ]; then 
-    exit 1
-fi
-EOF
-
-
-chmod +x .git/hooks/pre-push 
+chmod +x .git/hooks/pre-commit
